4c3b9cc6bf372b16db10f785ecf79ccf8bab101f
Egario54 <thierry.jacquot2@etu.univ-lorraine.fr>
2022-04-06 11:06:18 +0200

    Création de Telecommande.java

---
+++ Telecommande.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Telecommande {
+    private List<Lampe> lampes;
+
+    public Telecommande (){
+        this.lampes = new ArrayList<>();
+    }
+
+    public void ajouterLampe(Lampe lmp){
+        lampes.add(lmp);
+    }
+    public void activerLampe(int indiceLampe){
+        lampes.get(indiceLampe).allumer();
+    }
+    public void eteindreLampe(int indiceLampe){
+        lampes.get(indiceLampe).eteindre();
+    }
+    public void activerTout(){
+        for(int i = 0; i< lampes.size();i++){
+            lampes.get(i).allumer();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Telecommande{" +
+                "lampes=" + lampes +
+                '}';
+    }
+}
